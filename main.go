905594f1f25839961655c1853ee28a9d669205c8
cenan <cenan.ozen@gmail.com>
2024-05-24 16:12:32 +0300

    initial

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+// an echo application that returns hello world on /
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":8080"))
+}
